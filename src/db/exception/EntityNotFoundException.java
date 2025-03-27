739ca11d2767a92f6f8e1c9f7bd8a2995fdfd9c0
amirparsaro <amirparsarouhani@gmail.com>
2025-03-27 19:09:25 +0330

    created constructors for exception class EntityNotFoundException

--- src/db/exception/EntityNotFoundException.java
+++ src/db/exception/EntityNotFoundException.java
@@ -1,4 +1,15 @@
 package db.exception;
 
-public class EntityNotFoundException {
+public class EntityNotFoundException extends Exception {
+    EntityNotFoundException() {
+        super("Cannot find entity.");
+    }
+
+    EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(int id) {
+        super("Cannot find entity with id = " + id + ".");
+    }
 }

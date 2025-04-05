f66f32d0c90ad00fb82433c7fd622e61797831d5
amirparsaro <amirparsarouhani@gmail.com>
2025-04-05 14:16:23 +0330

    create class Step

---
+++ src/todo/entity/Step.java
@@ -0,0 +1,30 @@
+package todo.entity;
+
+import db.*;
+
+public class Step extends Entity {
+    @Override
+    public Entity copy() {
+        return null;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return 0;
+    }
+
+    enum Status {
+        NotStatred,
+        Completed
+    }
+
+    public String title;
+    public Status status;
+    public int taskRef;
+
+    public Step(String title, Status status, int taskRef) {
+        this.title = title;
+        this.status = status;
+        this.taskRef = taskRef;
+    }
+}

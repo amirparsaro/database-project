2eb08776e48d87c8e6320c3a936ba046e37ff2b8
amirparsaro <amirparsarouhani@gmail.com>
2025-04-11 20:31:07 +0330

    create method updateTake in TaskService

--- src/todo/service/StepService.java
+++ src/todo/service/StepService.java
@@ -3,14 +3,22 @@ package todo.service;
 import db.Database;
 import db.exception.InvalidEntityException;
 import todo.entity.Step;
-import todo.entity.Task;
 
 public class StepService {
     public static void saveStep(int taskRef, String title) {
-        Step newStep = new Step(title, Task.Status.NotStarted, taskRef);
+        Step newStep = new Step(title, Step.Status.NotStarted, taskRef);
 
         try {
             Database.add(newStep);
+        } catch (InvalidEntityException e) {
+            System.out.println("[Error] Entity is invalid. Check for any wrong input.");
+        }
+    }
+
+    public static void addStep(int taskId, String title) {
+        Step step = new Step(title, Step.Status.NotStarted, taskId);
+        try {
+            Database.add(step);
         } catch (InvalidEntityException e) {
             System.out.println("[Error] Entity is invalid. Check for any wrong input.");
         }
--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -1,8 +1,10 @@
 package todo.service;
 
 import java.util.Date;
+
 import db.Database;
 import db.Entity;
+import db.exception.EntityNotFoundException;
 import db.exception.InvalidEntityException;
 import todo.entity.Task;
 
@@ -16,23 +18,36 @@ public class TaskService {
 
         task.status = Task.Status.Completed;
 
-        try {
-            Database.update(task);
-        } catch (InvalidEntityException e) {
-            System.out.println("[Error] Entity is invalid. Check for any wrong input.");
-        }
+        Database.update(task);
+
     }
 
-    public static void addTask(String title, String description, String due) {
+    public static void addTask(String title, String description, String due) throws InvalidEntityException {
         String[] splitDueDate = due.split("-");
-        Date dueDate = new Date(Integer.parseInt(splitDueDate[0]), Integer.parseInt(splitDueDate[1]),
+        Date dueDate = new Date(Integer.parseInt(splitDueDate[0]) - 1900, Integer.parseInt(splitDueDate[1]) - 1,
                 Integer.parseInt(splitDueDate[2]));
 
-        Task task = new Task(title, description, dueDate);
-        try {
-            Database.add(task);
-        } catch (InvalidEntityException e) {
-            System.out.println("[Error] Entity is invalid. Check for any wrong input.");
+        Task task = new Task(title, description, dueDate, Task.Status.NotStarted);
+        Database.add(task);
+    }
+
+    public static void updateTask(int taskId, String field, String newValue)
+            throws InvalidEntityException, EntityNotFoundException, IllegalArgumentException {
+        Task task = (Task) Database.get(taskId);
+
+        if (field.equals("title")) {
+            task.title = newValue;
+        } else if (field.equals("description")) {
+            task.description = newValue;
+        } else if (field.equals("due-date")) {
+            String[] splitDueDate = newValue.split("-");
+            Date dueDate = new Date(Integer.parseInt(splitDueDate[0]) - 1900, Integer.parseInt(splitDueDate[1]) - 1,
+                    Integer.parseInt(splitDueDate[2]));
+            task.dueDate = dueDate;
+        } else {
+            throw new IllegalArgumentException("Unknown field detected.");
         }
+
+        Database.update(task);
     }
 }

78ef9f67f67fbb01f328e65ca7b37f3cacdf5fa2
amirparsaro <amirparsarouhani@gmail.com>
2025-04-11 20:44:27 +0330

    create method updateStep in StepService

--- src/todo/service/StepService.java
+++ src/todo/service/StepService.java
@@ -1,26 +1,41 @@
 package todo.service;
 
 import db.Database;
+import db.Entity;
+import db.exception.EntityNotFoundException;
 import db.exception.InvalidEntityException;
 import todo.entity.Step;
+import todo.entity.Task;
+
+import java.util.Date;
 
 public class StepService {
-    public static void saveStep(int taskRef, String title) {
+    public static void saveStep(int taskRef, String title) throws InvalidEntityException {
         Step newStep = new Step(title, Step.Status.NotStarted, taskRef);
 
-        try {
-            Database.add(newStep);
-        } catch (InvalidEntityException e) {
-            System.out.println("[Error] Entity is invalid. Check for any wrong input.");
-        }
+        Database.add(newStep);
+
     }
 
-    public static void addStep(int taskId, String title) {
-        Step step = new Step(title, Step.Status.NotStarted, taskId);
-        try {
-            Database.add(step);
-        } catch (InvalidEntityException e) {
-            System.out.println("[Error] Entity is invalid. Check for any wrong input.");
+    public static String updateStep(int stepId, String field, String newValue)
+            throws InvalidEntityException, EntityNotFoundException, IllegalArgumentException {
+        String oldValue = "";
+
+        Entity entity = Database.get(stepId);
+        if (!(entity instanceof Task)) {
+            throw new InvalidEntityException("Entity is not an instance of Task.");
         }
+        Step step = (Step) entity;
+
+        if (field.equals("title")) {
+            oldValue = step.title;
+            step.title = newValue;
+        } else {
+            throw new IllegalArgumentException("Unknown field detected.");
+        }
+
+        Database.update(step);
+
+        return oldValue;
     }
 }
